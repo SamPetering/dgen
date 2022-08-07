d4f3170e09e57c8aec6a63f667ad2d7b4cc59ec8
Sam Petering <samuel.petering@gmail.com>
2022-08-06 20:46:07 -0700

    basic auth flow

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,18 +2,33 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Login = ({}) => {
   const { data: session } = useSession();
+  const btnStyle =
+    'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
   if (session) {
+    console.log({ user: session.user });
     return (
       <>
         <p>Welcome, {session.user?.email}</p>
-        <button onClick={() => signOut()}>Sign out</button>
+        {!!session?.user?.image && (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={session.user?.image}
+            alt=""
+            style={{ borderRadius: '50px' }}
+          />
+        )}
+        <button className={btnStyle} onClick={() => signOut()}>
+          Sign out
+        </button>
       </>
     );
   } else {
     return (
       <>
         <p>You are not signed in.</p>
-        <button onClick={() => signIn()}>Sign in</button>
+        <button className={btnStyle} onClick={() => signIn()}>
+          Sign in
+        </button>
       </>
     );
   }
