d1a342901a605fd46bebe313564d7177c051d45e
Sam Petering <samuel.petering@gmail.com>
2022-08-20 12:10:52 -0700

    nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,16 @@
 import { useSession } from 'next-auth/react';
-import { HTMLAttributes } from 'react';
-
-type NavButtonProps = {
-  linkTo: string;
-  onClick?: () => void;
-} & HTMLAttributes<HTMLAnchorElement>;
+import { NavButton } from '../NavButton';
 
 const Header = () => {
   const { data: session } = useSession();
-  const NavButton = ({ children, linkTo, ...props }: NavButtonProps) => (
-    <div className="px-2 text-yellow-20">
-      <a {...props} href={linkTo}>
-        {children}
-      </a>
-    </div>
-  );
+
   return (
-    <div className="text-yellow-200 bg-yellow-900 w-full h-12 flex justify-between">
-      <NavButton linkTo="/" className="font-bold h-full flex items-center px-4">
-        dgen
-      </NavButton>
+    <div className="bg-yellow-500 w-full h-12 flex justify-between">
+      <NavButton linkTo="/" text="dgen" />
 
       <div className="flex h-full items-center">
-        {!session && <NavButton linkTo="/login">login</NavButton>}
-        {!!session && <NavButton linkTo="/profile">profile</NavButton>}
+        {!session && <NavButton linkTo="/login" text="login" />}
+        {!!session && <NavButton linkTo="/profile" text="profile" />}
       </div>
     </div>
   );
