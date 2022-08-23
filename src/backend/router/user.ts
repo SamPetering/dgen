f21f880f9d72e39a72f0c83e7f8ca428421a7fa1
Sam Petering <samuel.petering@gmail.com>
2022-08-22 22:39:25 -0700

    user mutation working

--- src/backend/router/user.ts
+++ src/backend/router/user.ts
@@ -2,19 +2,45 @@ import * as trpc from '@trpc/server';
 import { prisma } from '../../db/client';
 import * as z from 'zod';
 
-export const userRouter = trpc.router().query('create', {
-  input: z.object({
-    email: z.string({
-      required_error: 'Email is required',
+export const userRouter = trpc
+  .router()
+  .mutation('create', {
+    input: z.object({
+      email: z
+        .string({
+          required_error: 'Email is required',
+        })
+        .min(5),
+      name: z.string().optional(),
     }),
-    name: z.string().optional(),
-  }),
-  async resolve({ input }) {
-    return await prisma.user.create({
-      data: {
-        email: input.email,
-        name: input.name,
-      },
-    });
-  },
-});
+    async resolve({ input }) {
+      return await prisma.user.create({
+        data: {
+          email: input.email,
+          name: input.name,
+        },
+      });
+    },
+  })
+  .mutation('updateProfile', {
+    input: z.object({
+      userId: z.number({
+        required_error: 'User Id is required',
+      }),
+      age: z.number({
+        required_error: 'Age is required',
+      }),
+      height: z.number().optional(),
+      weight: z.number().optional(),
+    }),
+    async resolve({ input }) {
+      return await prisma.userProfile.create({
+        data: {
+          userId: input.userId,
+          age: input.age,
+          height: input.height,
+          weight: input.weight,
+        },
+      });
+    },
+  });
