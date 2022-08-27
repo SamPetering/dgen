75b9445cb00ad8377a9f1d87ceebb26b78f2ebb1
Sam Petering <samuel.petering@gmail.com>
2022-08-26 19:34:38 -0700

    remove unused code

--- src/backend/router/user.ts
+++ src/backend/router/user.ts
@@ -2,45 +2,4 @@ import * as trpc from '@trpc/server';
 import { prisma } from '../../db/client';
 import * as z from 'zod';
 
-export const userRouter = trpc
-    .router()
-    .mutation('create', {
-        input: z.object({
-            email: z
-                .string({
-                    required_error: 'Email is required',
-                })
-                .min(5),
-            username: z.string().optional(),
-        }),
-        async resolve({ input }) {
-            return await prisma.user.create({
-                data: {
-                    email: input.email,
-                    username: input.username,
-                },
-            });
-        },
-    })
-    .mutation('updateProfile', {
-        input: z.object({
-            userId: z.number({
-                required_error: 'User Id is required',
-            }),
-            age: z.number({
-                required_error: 'Age is required',
-            }),
-            height: z.number().optional(),
-            weight: z.number().optional(),
-        }),
-        async resolve({ input }) {
-            return await prisma.userProfile.create({
-                data: {
-                    userId: input.userId,
-                    age: input.age,
-                    height: input.height,
-                    weight: input.weight,
-                },
-            });
-        },
-    });
+export const userRouter = trpc.router();
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,34 +4,10 @@ import { useState } from 'react';
 import PrimaryInput from '../components/PrimaryInput';
 
 const Login = ({}) => {
-    const [emailOrUsername, setEmailOrUsername] = useState('');
-    const [password, setPassword] = useState('');
     const btnStyle =
         'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
     return (
         <>
-            {/* <PrimaryInput
-                placeholder="email or username"
-                onChange={(e) => setEmailOrUsername(e.target.value)}
-                value={emailOrUsername}
-            />
-            <PrimaryInput
-                placeholder="password"
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                value={password}
-            />
-            <button
-                className={btnStyle}
-                onClick={() =>
-                    signIn('credentials', {
-                        emailOrUsername,
-                        password,
-                    })
-                }
-            >
-                Sign in
-            </button> */}
             <button className={btnStyle} onClick={() => signIn('google')}>
                 Sign in with google
             </button>
