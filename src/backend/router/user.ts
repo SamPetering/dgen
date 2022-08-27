9bb97605d14c21cf543d67171d0946c71346382c
Sam Petering <samuel.petering@gmail.com>
2022-08-26 20:22:34 -0700

    add user profile

--- src/backend/router/user.ts
+++ src/backend/router/user.ts
@@ -2,4 +2,25 @@ import * as trpc from '@trpc/server';
 import { prisma } from '../../db/client';
 import * as z from 'zod';
 
-export const userRouter = trpc.router();
+export const userRouter = trpc.router().mutation('updateProfile', {
+    input: z.object({
+        userId: z.string({
+            required_error: 'User Id is required',
+        }),
+        age: z.number({
+            required_error: 'Age is required',
+        }),
+        height: z.number().optional(),
+        weight: z.number().optional(),
+    }),
+    async resolve({ input }) {
+        return await prisma.userProfile.create({
+            data: {
+                userId: input.userId,
+                age: input.age,
+                height: input.height,
+                weight: input.weight,
+            },
+        });
+    },
+});
